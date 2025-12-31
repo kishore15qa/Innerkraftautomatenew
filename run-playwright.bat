b446d6cbc7135fd5c168fab92a642e7e7cfed987
Kishore1561999 <rskishore1561999@gmail.com>
2025-12-31 15:30:06 +0530

    Fifth commit

--- run-playwright.bat
+++ run-playwright.bat
@@ -1,11 +1,20 @@
 @echo on
+echo ==== BAT FILE STARTED ====
+
 cd /d %WORKSPACE%
-echo Current directory:
-cd
+echo ==== AFTER CD ====
 
 node -v
+echo ==== AFTER NODE ====
+
 npm -v
+echo ==== AFTER NPM ====
 
 npm install
+echo ==== AFTER NPM INSTALL ====
+
 npx playwright install
+echo ==== AFTER PW INSTALL ====
+
 npx playwright test
+echo ==== AFTER TEST ====
